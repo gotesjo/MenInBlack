eb13b09b4ee9547d80deaec5d6e6384c1fb2dfbd
gotesjo <120645559+gotesjo@users.noreply.github.com>
2022-12-20 11:22:44 +0100

    Efter Java Package

--- MenInBlack/src/MIB/Inlog.java
+++ MenInBlack/src/MIB/Inlog.java
@@ -1,3 +1,5 @@
+package MIB;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
