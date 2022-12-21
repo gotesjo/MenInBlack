e9bd2323fb78de313ba83ff1278079cc8c8e2fd0
gotesjo <120645559+gotesjo@users.noreply.github.com>
2022-12-21 15:43:30 +0100

    Update User.java

--- MenInBlack/src/meninblack/User.java
+++ MenInBlack/src/meninblack/User.java
@@ -4,7 +4,9 @@
  */
 package meninblack;
 
+import javax.swing.JOptionPane;
 import oru.inf.InfDB;
+import oru.inf.InfException;
 
 /**
  *
@@ -33,5 +35,44 @@ public class User {
         return username;
     }
     
+    public String getPlats(){
+        
+        //return plats;
+    }
+    
+    /**
+     * Retunerar en String av Inloggad användares Områdeschef
+     * @return 
+     * Get metod som inte tar några parametrar.
+     * 
+     */
+    public String getOmradeschef(){
+        String chef = "";
+        
+        String sqlFraga = """
+                          SELECT Agent.namn FROM Agent
+                          join Omradeschef on Agent.Agent_ID = Omradeschef.Agent_ID
+                          join Omrade on Omradeschef.Omrade = Omrade.Omrades_ID
+                          join plats on  Omrade.Omrades_ID = plats.Finns_I
+                          join Alien on plats.Plats_ID = Alien.Plats
+                          WHERE Alien.Namn like '""" + username + "'";
+        
+        try{
+        chef = idb.fetchSingle(sqlFraga);
+        
+        } catch(InfException e){
+            
+            JOptionPane.showMessageDialog(null, "Något är lurt");
+            System.out.println("Internt Felmeddelande" + Undantag.getMessage());
+        }
+        
+        
+        return chef;
+    }
+    
+    
+    public String getOmrade(){
+        
+    }
     
 }
